c2b789a20883056f1f0dc213af33ac33453b0d36
renkeles <jetmebass@gmail.com>
2021-12-03 13:45:32 +0500

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
+<<<<<<< HEAD
+=======
+#include <cmath>
+>>>>>>> f7f8d2d (Initial commit)
 #include <iostream>
 #include <vector>
 #include <deque>
 #include <list>
+<<<<<<< HEAD
+=======
+#include <algorithm>
+#include <random>
+#include <ctime>
+>>>>>>> f7f8d2d (Initial commit)
 
 template <class Container, class T>
 void insert_sorted(Container& container, const T& item) {
@@ -18,6 +28,21 @@ void printContainer(Container& container, std::string name) {
     std::cout << std::endl;
 }
 
+<<<<<<< HEAD
+=======
+std::vector<float> operator-(const std::vector<float> &a, const std::vector<int> &b){
+
+    if (a.size() != b.size())
+        throw("a.size() != b.size()");
+    std::vector<float> c(a.size());
+    for(int i{0}; i < a.size(); ++i){
+        c[i] = a[i] - static_cast<float>(b[i]);
+        c[i] = sqrt(c[i]);
+    }
+    return c;
+}
+
+>>>>>>> f7f8d2d (Initial commit)
 void task_1() {
     std::vector<int> vec_int{ 1,2,3,4,5 };
     insert_sorted(vec_int, 4);
@@ -46,6 +71,32 @@ void task_1() {
 
 int main()
 {
+<<<<<<< HEAD
     task_1();
+=======
+    //task_1();
+
+    std::mt19937 gen(time(0));
+    std::uniform_real_distribution<> urd(0, 100);
+
+    std::vector<float> analogSignal(100);
+    std::vector<int> digitalSignal(100);
+
+    std::generate(analogSignal.begin(), analogSignal.end(), [&urd, &gen]() -> float{
+        return urd(gen);
+    });
+
+    std::copy(analogSignal.begin(), analogSignal.end(), digitalSignal.begin());
+
+    printContainer(analogSignal, "analog");
+    printContainer(digitalSignal, "digital");
+
+    std::vector<float> tmp(analogSignal.size());
+    tmp = analogSignal - digitalSignal; //(a - b)^2
+
+    auto sum = std::accumulate(tmp.begin(), tmp.end(), 0.0);
+
+    std::cout << "Sum = " << sum;
+>>>>>>> f7f8d2d (Initial commit)
 
 }
